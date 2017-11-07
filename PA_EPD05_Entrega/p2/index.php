051ec29eae0668450f5e2e1170e26c5e2aa0bfa1
Victor Martinelli <32304111+Victor-martinelli@users.noreply.github.com>
2017-11-07 14:42:15 +0100

    Fixed P2 to include seconds in Time Span

--- PA_EPD05_Entrega/p2/index.php
+++ PA_EPD05_Entrega/p2/index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+Grupo 7
 -->
 <html>
     <head>
@@ -15,7 +13,7 @@ and open the template in the editor.
         function getDateFormat() //Formato correcto para los archivos
         {
             $now = new DateTime();
-         return $now->format('Y-m-d_H-i');
+         return $now->format('Y-m-d_H-i-s');
         }
 
         function getImage($raw_tags, $filename) {
