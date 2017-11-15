f52360903efe68b7a31ae89f4d145ab2aefe79c5
Victor-martinelli <vicbomlen@gmail.com>
2017-11-15 19:30:01 +0100

    :b:inished e:b:1

---
+++ PA_EPD07_Entrega/ej1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+Group 7
+-->
+<html>
+    <head>
+        <script type="text/javascript">
+            <!--
+                function calculatepH(number)
+                {
+                    return -(Math.log10(number));
+                }
+            //-->
+            </script>
+    </head>
+    <body>
+            <script type="text/javascript">
+                <!--
+                    var number = prompt("Please enter a number","");
+                    var result = calculatepH(number);
+                    alert("The pH of "+ number + " is "+ result);
+                //-->
+                </script>
+    </body>
+</html>
