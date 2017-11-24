27226297003d5865d03548602f4402e06e38c00a
Manuel <mridpin@alu.upo.es>
2017-11-24 17:30:38 +0100

    p3 es una mierda

--- PA_EPD08_Entrega/p3/public_html/battleships.js
+++ PA_EPD08_Entrega/p3/public_html/battleships.js
@@ -1,9 +1,18 @@
 
 
+//GLOBAL VARIABLES  
 var size = 5; //Only square boards allowed because we are not savages
 var boardContainer = document.getElementById("boardSection");
+/* board will hold the following values:
+ *  0: water
+ *  1: tile with new ship or ship part
+ *  2. tile with existing ship
+ */
 var board = initializeBoard([size, size]);
-
+var shipCounter = document.getElementById("shipCounter");
+var shipCounterText = document.createTextNode("gfg");
+shipCounter.appendChild(shipCounterText);
+var maxShips = 6;
 /*Global variable for button clicked:
  *  0: nothing selected
  *  1: water selected
@@ -11,17 +20,18 @@ var board = initializeBoard([size, size]);
  */
 var buttonClicked = 0;
 
-drawBoard(board, boardContainer, size);
+/*Initialize board. "Gameplay" is event driven, so there is no action or game loop*/
+drawBoard(boardContainer, size);
 createButton("water.png", "waterButton");
 createButton("battleship.png", "battleshipButton");
 
-function drawBoard(board, boardContainer, size) {
+function drawBoard(boardContainer, size) {
     var table = document.createElement("table");
     for (var y = 0; y < size; y++) {
         var row = document.createElement("tr");
         for (var x = 0; x < size; x++) {
             var cell = document.createElement("td");
-            // Doubly nested functions are needed so that each cell uses itself as parameter. 
+            // Doubly nested functions are needed because of the for loop, so that each cell uses itself as parameter. 
             // If only one nested function, the parameter is always the last cell
             cell.addEventListener("click", function (pos) {
                 return function () {
@@ -102,15 +112,63 @@ function paintBoard(pos) {
     x = pos.cellIndex;
     y = pos.parentNode.rowIndex;
     figure = pos.firstChild;
+    //Check what are we painting, then update the image, then update the matrix
     if (buttonClicked === 1) {
         var img = document.createElement("img");
         img.setAttribute("src", "water.png");
         img.setAttribute("width", "50");
         figure.replaceChild(img, figure.firstChild);
+        board[y][x] = 0;
     } else if (buttonClicked === 2) {
         var img = document.createElement("img");
         img.setAttribute("src", "battleship.png");
         img.setAttribute("width", "50");
         figure.replaceChild(img, figure.firstChild);
+        board[y][x] = 1;
     }
+    if (checkShipIntegrity(maxShips, shipCounter)) {
+        alert("Maximum ships placed");
+    }
+}
+function checkShipIntegrity(maxShipsAllowed, currentShipCounter) {
+    //Step 1: count ships and update visual counter
+    var currentShips = countShips(maxShipsAllowed);
+    //alert(currentShips);
+    var currentShipsText = document.createTextNode(currentShips.toString());
+    currentShipCounter.replaceChild(currentShipsText, currentShipCounter.firstChild);
+
+    return currentShips === maxShipsAllowed;
+}
+function checkShipsLength() {
+
+}
+
+function countShips(maxShipsAllowed) {
+    /*To count ships, we iterate horizontally. If we find a 1, we start "filling" a ship. If the next place is 0, 
+     * the ship is reset. If the next place is a 1, we fill the ship until a 0 or a board limit is found. 
+     * Then, we do the same vertically. This method does not allow 1 piece ships, and if two ships are in T or L shape,
+     * the horizontal one will be longer  */
+    var maxShipSize = parseInt(document.getElementById("shipSize").value);
+    var shipCounter = 0;
+    var ship = 0; //"empty ship"
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            if (board[i][j] === 1) {
+                // A new ship piece has been found, build a ship
+                ship += 1;
+            }
+            if (board[i][j] === 0 && ship === 1) {
+                // A vertical ship has been found, reset the ship counter
+                ship = 0;
+            }
+            if (ship === maxShipSize) {
+                shipCounter += 1;
+                ship = 0;
+            }
+
+        }
+        // Board edge reached, reset the ship builder:
+        ship = 0;
+    }
+    return shipCounter;
 }
