f8bf98cc7136ebe57247d35b83acf3036f7f3f28
mridpin <mridao99@gmail.com>
2017-12-06 20:49:15 +0100

    p4 finished

--- PA_EPD08_Entrega/p4/public_html/program.js
+++ PA_EPD08_Entrega/p4/public_html/program.js
@@ -5,6 +5,7 @@ var gameDiv = document.getElementById("game");
 var errorsDiv = document.getElementById("errors");
 var input = document.getElementById("input");
 var MAX_ERRORS = 5;
+var currentErrors = 0;
 
 startGame(solutionDiv, gameDiv, errorsDiv);
 
@@ -14,12 +15,11 @@ function startGame(solutionDiv, gameDiv, errorsDiv) {
     createSolutionDiv(solutionDiv, word);
     createGameDiv(gameDiv, word);
     createErrorCounter(errorsDiv, word);
-    var currentErrors = 0;
     var input = document.getElementById("input");
-    input.addEventListener("keypress", function () {
-        return processKey(event, word, currentErrors);
-    });
-    
+    input.onkeyup = function () {
+        return processKey(this, word);
+    };
+
 }
 
 function createSolutionDiv(solutionDiv, word) {
@@ -36,7 +36,6 @@ function createSolutionDiv(solutionDiv, word) {
 }
 
 function hideAndRevealOnHover(hiddenWord) {
-    // Make it invisible
     hiddenWord.style.color = "white";
     hiddenWord.onmouseover = function () {
         makeVisible(this);
@@ -70,16 +69,74 @@ function createErrorCounter(errorsDiv, word) {
     var p = document.createElement("p");
     var text = document.createTextNode("Errors: ");
     var errorCount = document.createElement("span");
-    errorCount.appendChild(document.createTextNode(""));
+    errorCount.id = "errorCount";
+    errorCount.appendChild(document.createTextNode("0"));
     p.style.border = "2px solid black";
     p.appendChild(text);
     p.appendChild(errorCount);
     errorsDiv.appendChild(p);
 }
 
-function processKey(event, word, currentErrors) {    
-    var letter = event.keyCode;
-    
-    
-    
+function processKey(input, word) {
+    // Captures the value of the input and validates it as good or error. Deletes it afterwards
+    var letter = input.value;
+    if (letter === "") {
+        alert("key not valid");
+    } else {
+        var occurrences = getAllIndices(word, letter);
+        if (occurrences.length === 0) {
+            errorUp();
+        } else {
+            drawLetter(letter, occurrences);
+        }
+        // Delete the value for next time value
+        input.value = "";
+        if (checkIfWin(word)) {
+            alert("YOU WON");
+        }
+    }
+
+}
+
+function getAllIndices(word, letter) {
+    // Returns an array with all occurrences of letter in word: https://stackoverflow.com/questions/20798477/how-to-find-index-of-all-occurrences-of-element-in-array
+    var indices = []
+    var i = -1;
+    while ((i = word.indexOf(letter, i + 1)) !== -1) {
+        indices.push(i);
+    }
+    return indices;
+}
+
+function drawLetter(letter, occurrences) {
+    // Prints letter in the corresponding cell in the table by iterating through occurrences
+    var resultRow = document.getElementById("results");
+    var cells = resultRow.childNodes;
+    for (var x in occurrences) {
+        if (cells[occurrences[x] + 1].textContent !== letter) {
+            cells[occurrences[x] + 1].appendChild(document.createTextNode(letter)); //index+1 because first child is text
+        }
+    }
+}
+
+function errorUp() {
+    currentErrors = currentErrors + 1;
+    var errCounter = document.getElementById("errorCount");
+    errCounter.textContent = currentErrors.toString();
+    if (currentErrors === MAX_ERRORS) {
+        alert("YOU LOST");
+    }
+}
+
+function checkIfWin(word) {
+    var cells = document.getElementById("results").childNodes;
+    var res = true;
+    var i = 0;
+    while (i<word.length && res) {
+        if (cells[i].textContent === "") {
+            res = false;
+        }
+        i++;
+    }
+    return res;
 }
